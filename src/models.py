4f28c0d37beeb438d55f905976c171f0a05d896b
sebaaguayo <sebaaguayoc@gmail.com>
2023-06-30 17:48:03 +0000

    instadatabaselisto

--- src/models.py
+++ src/models.py
@@ -1,29 +1,82 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
+from sqlalchemy.orm import relationship, declarative_base, backref
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
+    id =  Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    bio = Column(String(160))
+    website = Column(String(100))
+    profile_picture_url = Column(String(100))
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    is_active = Column(Boolean, default=True)
+    post = relationship('Posts', backref='user')
+    media = relationship('Media', backref='user')
+    followers = relationship('Follow', backref='following', foreign_keys='Follow.following_id')
+    following = relationship('Follow', backref='followers', foreign_keys='Follow.follower.id')
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__= 'posts'
+    id = Column (Integer, primary_key=True)
+    caption = Column(String(2200))
+    image_url = Column(String(100))
+    location = Column (String(100))
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    likes = relationship('Like', backref='post')
+    comments = relationship ('Comment', backref='post')
+
+class Comment(Base):
+    __tablename__= 'comments'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    text = Column(String(1000))
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    post_id= Column(Integer, ForeignKey('posts.id'))
+    user_id= Column(Integer, ForeignKey('users.id'))
+    
+
+class Like(Base):
+    __tablename__= 'likes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user = relationship(User, backref='likes')
+    post = relationship(Post, backref='likes')
+
+
+
+
+class Media(Base):
+    __tablename__='media'
+    id= Column(Integer, primary_key=True)
+    type = Column(String(50), nullable=False)
+    url = Column(String(100), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, nullable=False)
+
+class Follow(Base):
+    __tablename__= 'follows'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False)
+    follower_id = Column(Integer, ForeignKey('users.id'))
+    following_id = Column(Integer, ForeignKey('users.id'))
+
 
     def to_dict(self):
         return {}
